65ddbb05016e7d14a5f8c03aef4bebe4570d55ef
Javier Vega <vega@debian>
2021-11-23 12:53:12 +0100

    migración

---
+++ apps/text/js/highlight/erb.js
@@ -0,0 +1,2 @@
+(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[55],{509:function(n,e,u){"use strict";n.exports=function(n){return{name:"ERB",subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+//# sourceMappingURL=erb.js.map?v=40bd9288cc8caa17c6dc
---
+++ version.php
@@ -0,0 +1,20 @@
+<?php 
+$OC_Version = array(22,2,0,2);
+$OC_VersionString = '22.2.0';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '21.0' => true,
+    '22.0' => true,
+    '22.1' => true,
+    '22.2' => true,
+  ),
+  'owncloud' => 
+  array (
+    '10.5' => true,
+  ),
+);
+$OC_Build = '2021-09-29T21:21:53+00:00 07e359b7de671a81a7baad701d4496a9f871a770';
+$vendor = 'nextcloud';
